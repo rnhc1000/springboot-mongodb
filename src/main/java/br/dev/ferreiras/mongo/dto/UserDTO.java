e91d72a75dd418021dbc04fa9900d666e239cbc8
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-15 14:29:30 -0300

    Added UserDTO and adjusted UserResource to return a UserDTO

---
+++ src/main/java/br/dev/ferreiras/mongo/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package br.dev.ferreiras.mongo.dto;
+
+import java.io.Serializable;
+
+import br.dev.ferreiras.mongo.domain.User;
+
+public class UserDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+	private String name;
+	private String email;
+
+	public UserDTO() {
+	}
+
+	public UserDTO(User obj) {
+
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
--- src/main/java/br/dev/ferreiras/mongo/resources/UserResource.java
+++ src/main/java/br/dev/ferreiras/mongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package br.dev.ferreiras.mongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.dev.ferreiras.mongo.domain.User;
+import br.dev.ferreiras.mongo.dto.UserDTO;
 import br.dev.ferreiras.mongo.services.UserService;
 
 @RestController
@@ -19,14 +21,15 @@ public class UserResource {
 	private UserService userService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 
 //		User maria = new User("1", "Maria Brown", "maria@gmail.com");
 //		User alex = new User("2", "Alex Green", "alex@gmail.com");
 
 		List<User> list = userService.findAll();
 //		list.addAll(Arrays.asList(maria, alex));
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
