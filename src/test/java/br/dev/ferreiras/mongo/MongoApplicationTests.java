c8f4c5e4d846bb759f5aaa5009da2a5ac06a583c
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-14 23:02:36 -0300

    Setting up project

---
+++ src/test/java/br/dev/ferreiras/mongo/MongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.ferreiras.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
